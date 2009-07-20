72ce36f78a95409893c0f53e155d9b057fb3db08
effervex <effervex@00e55cea-62a9-11de-89e8-69fd2269e136>
2009-07-20 02:05:15 +0000

    Version 1.0 of the algorithm, where hand-coded and random policies are working.

---
+++ MsPacMan/MsPacMan/src/rlPacMan/RunPacManExperiment.java
@@ -0,0 +1,26 @@
+package rlPacMan;
+
+import org.rlcommunity.rlglue.codec.*;
+
+public class RunPacManExperiment {
+	public static void main(String[] args) {
+		// Create the Agent
+		AgentInterface theAgent = new PacManAgent();
+
+		// Create the Environment
+		EnvironmentInterface theEnvironment = new PacManEnvironment();
+
+		LocalGlue localGlueImplementation = new LocalGlue(theEnvironment,
+				theAgent);
+		RLGlue.setGlue(localGlueImplementation);
+
+		// Run the main method of the Sample Experiment, using the arguments
+		// were were passed
+		// This will run the experiment in the main thread. The Agent and
+		// Environment will run
+		// locally, without sockets.
+		//PacManExperiment.main(args);
+		CrossEntropyExperiment.main(args);
+		System.out.println("RunMinesSarsaExperimentNoSockets Complete");
+	}
+}
