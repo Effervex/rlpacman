5c548e39001b8e1d2baebfa29afe61fb16afebe7
effervex <effervex@00e55cea-62a9-11de-89e8-69fd2269e136>
2011-01-05 22:21:34 +0000

    Swapped the Gaussian distribution for updating probabilities to a Poisson distribution, which should hopefully work out the same or better. It removes the need for observing variance anyway.

--- trunk/MsPacMan/src/test/TestTest.java
+++ trunk/MsPacMan/src/test/TestTest.java
@@ -2,13 +2,16 @@ package test;
 
 import static org.junit.Assert.*;
 
+import org.apache.commons.math.distribution.PoissonDistribution;
+import org.apache.commons.math.distribution.PoissonDistributionImpl;
 import org.junit.Test;
-
-import relationalFramework.Number;
 
 public class TestTest {
 	@Test
-	public void testStuff() {
-		
+	public void testStuff() throws Exception {
+		PoissonDistribution p = new PoissonDistributionImpl(1);
+		assertEquals(p.inverseCumulativeProbability(0) + 1, 0);
+		assertEquals(p.inverseCumulativeProbability(0.5) + 1, 1);
+		assertEquals(p.inverseCumulativeProbability(0.75) + 1, 2);
 	}
 }
