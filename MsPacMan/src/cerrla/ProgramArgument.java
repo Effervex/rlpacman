e26b87fd6e0e1b021db767da73319c7a268393d3
effervex@gmail.com <effervex@gmail.com@00e55cea-62a9-11de-89e8-69fd2269e136>
2011-07-26 05:32:36 +0000

    A few more tweaks to group program arguments together

--- MsPacMan/src/cerrla/ProgramArgument.java
+++ MsPacMan/src/cerrla/ProgramArgument.java
@@ -7,56 +7,68 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 public enum ProgramArgument implements Serializable {
-	ALPHA(0.6, "alpha", null, "Step size update"),
-	BETA(0.01, "beta", null, "If KL sum updates are less than Beta * Alpha"),
-	CHI(0.1, "chi", null, "The resampling percentage of average episode steps"),
-	DYNAMIC_SLOTS(true, "dynamicSlots", null, "If the slots "
-			+ "grow dynamically"),
-	ENSEMBLE_EVALUATION(false, "ensembleEvaluation", null, "If using "
-			+ "ensemble evaluation"),
-	ENSEMBLE_SIZE(100, "ensembleSize", null, "The size of the ensemble"),
-	INITIAL_ORDERING_SD(0.25, "initialSlotOrderingSD", null, "The SD of "
-			+ "the slot order"),
-	INITIAL_SLOT_MEAN(1, "initialSlotMean", "-slotProb", "The initial "
-			+ "slot mu probabilities. -1 means use 1/D_S"),
-	LOCAL_ALPHA(true, "localAlpha", null, "If updates are performed "
-			+ "slot locally"),
-	MIN_WEIGHTED_UPDATE(0.1, "minWeightedUpdate", null, "The minimal update "
-			+ "when using weighted updates"),
-	NUM_NUMERICAL_SPLITS(3, "numNumericalSplits", null, "The number of "
-			+ "numerical splits"),
-	NUM_PERFORMANCES_CONVERGED(3, "numPerformancesConverged", null, "If "
-			+ "the average performance has remained the same for "
-			+ "this many iterations"),
-	NUM_UPDATES_CONVERGED(10, "numUpdatesConverged", null, "If KL sum "
-			+ "updates remain below Beta for this many updates"),
-	ONLY_GOAL_RULES(false, "onlyGoalRules", "-goalRules", "If the agent should "
-			+ "only create rules with the goal condition in it"),
-	PERFORMANCE_EPISODE_GAP(100, "performanceEpisodeGap", null, "The gap "
-			+ "between measuring performances"),
-	PERFORMANCE_TESTING_SIZE(100, "policyTestingSize", null, "Size of average "
-			+ "performance sliding window"),
-	POLICY_REPEATS(3, "policyRepeats", null, "Number of times "
-			+ "policy is repeated"),
-	RHO(0.05, "rho", null, "N_E's proportion of N"),
-	SLOT_THRESHOLD(-1, "slotThreshold", null, "The slot splitting threshold. "
-			+ "-1 means use |S|-1 threshold"),
-	TEST_ITERATIONS(100, "testIterations", null, "Number of iterations "
-			+ "to test the final testing for"),
-	USE_MODULES(true, "useModules", null, "If using/learning modules"),
-	WEIGHTED_UPDATES(false, "weightedUpdates", null, "If using "
-			+ "weighted updates");
+	ALPHA(0.6, "alpha", null, ParameterType.UPDATING, "Step size update"),
+	BETA(0.01, "beta", null, ParameterType.CONVERGENCE,
+			"If KL sum updates are less than Beta * Alpha"),
+	CHI(0.1, "chi", null, ParameterType.SAMPLING,
+			"The resampling percentage of average episode steps"),
+	DYNAMIC_SLOTS(true, "dynamicSlots", null, ParameterType.SPECIALISATION,
+			"If the slots grow dynamically"),
+	ENSEMBLE_EVALUATION(false, "ensembleEvaluation", null,
+			ParameterType.EVALUATION, "If using ensemble evaluation"),
+	ENSEMBLE_SIZE(100, "ensembleSize", null, ParameterType.EVALUATION,
+			"The size of the ensemble"),
+	INITIAL_ORDERING_SD(0.25, "initialSlotOrderingSD", null,
+			ParameterType.SAMPLING, "The SD of the slot order"),
+	INITIAL_SLOT_MEAN(1, "initialSlotMean", "-slotProb",
+			ParameterType.SAMPLING,
+			"The initial slot mu probabilities. -1 means use 1/D_S"),
+	LOCAL_ALPHA(true, "localAlpha", null, ParameterType.UPDATING,
+			"If updates are performed slot locally"),
+	MIN_WEIGHTED_UPDATE(0.1, "minWeightedUpdate", null, ParameterType.UPDATING,
+			"The minimal update when using weighted updates"),
+	NUM_NUMERICAL_SPLITS(3, "numNumericalSplits", null,
+			ParameterType.SPECIALISATION, "The number of numerical splits"),
+	NUM_PERFORMANCES_CONVERGED(3, "numPerformancesConverged", null,
+			ParameterType.CONVERGENCE,
+			"If the average performance has remained the same for this many iterations"),
+	NUM_UPDATES_CONVERGED(10, "numUpdatesConverged", null,
+			ParameterType.CONVERGENCE,
+			"If KL sum updates remain below Beta for this many updates"),
+	ONLY_GOAL_RULES(false, "onlyGoalRules", "-goalRules",
+			ParameterType.SPECIALISATION,
+			"If the agent should only create rules with the goal condition in it"),
+	PERFORMANCE_EPISODE_GAP(100, "performanceEpisodeGap", null,
+			ParameterType.EVALUATION, "The gap between measuring performances"),
+	PERFORMANCE_TESTING_SIZE(100, "policyTestingSize", null,
+			ParameterType.EVALUATION,
+			"Size of average performance sliding window"),
+	POLICY_REPEATS(3, "policyRepeats", null, ParameterType.EVALUATION,
+			"Number of times policy is repeated"),
+	RHO(0.05, "rho", null, ParameterType.UPDATING, "N_E's proportion of N"),
+	SLOT_THRESHOLD(-1, "slotThreshold", null, ParameterType.SPECIALISATION,
+			"The slot splitting threshold. -1 means use |S|-1 threshold"),
+	TEST_ITERATIONS(100, "testIterations", null, ParameterType.EVALUATION,
+			"Number of iterations to test the final testing for"),
+	USE_MODULES(true, "useModules", null, ParameterType.SAMPLING,
+			"If using/learning modules"),
+	WEIGHTED_UPDATES(false, "weightedUpdates", null, ParameterType.UPDATING,
+			"If using weighted updates");
 
 	public static final File ARG_FILE = new File("cerrlaArgs.txt");
 	private Boolean booleanValue_;
 	private String comment_;
 	private Object defaultValue_;
-	private Number numberValue_;
 	private String name_;
+	private Double numberValue_;
+	private ParameterType parameterType_;
 	private String shortcut_;
 
 	/**
@@ -72,9 +84,11 @@ public enum ProgramArgument implements Serializable {
 	 *            The comment about the argument.
 	 */
 	private ProgramArgument(Object defaultVal, String name, String shortcut,
-			String comment) {
+			ParameterType pType, String comment) {
 		if (defaultVal instanceof Number) {
-			numberValue_ = (Number) defaultVal;
+			if (defaultVal instanceof Integer)
+				defaultVal = ((Integer) defaultVal).doubleValue();
+			numberValue_ = (Double) defaultVal;
 			defaultValue_ = numberValue_;
 		} else if (defaultVal instanceof Boolean) {
 			booleanValue_ = (Boolean) defaultVal;
@@ -83,6 +97,7 @@ public enum ProgramArgument implements Serializable {
 		name_ = name;
 		shortcut_ = shortcut;
 		comment_ = comment;
+		parameterType_ = pType;
 	}
 
 	public boolean booleanValue() {
@@ -137,6 +152,45 @@ public enum ProgramArgument implements Serializable {
 	}
 
 	/**
+	 * Handles a program argument not caught by the algorithm earlier.
+	 * 
+	 * @param i
+	 *            The current index.
+	 * @param args
+	 *            The arguments given at command-line.
+	 * @return The index after handling.
+	 */
+	public static int handleArg(int i, String[] args) {
+		if (args[i].equals("-slotProb")) {
+			i++;
+			if (args[i].equals("dynamic"))
+				INITIAL_SLOT_MEAN.setDoubleValue(-1);
+			else
+				INITIAL_SLOT_MEAN.setDoubleValue(Double.parseDouble(args[i]));
+		} else if (args[i].equals("-ensemble")) {
+			ENSEMBLE_EVALUATION.setBooleanValue(true);
+			i++;
+			ENSEMBLE_SIZE.setDoubleValue(Integer.parseInt(args[i]));
+		} else if (args[i].equals("-dynamicSlots")) {
+			i++;
+			DYNAMIC_SLOTS.setValue(args[i]);
+		} else if (args[i].charAt(0) == '-') {
+			// Check the arg against the rest of the program args
+			for (ProgramArgument pa : ProgramArgument.values()) {
+				// If the arg equals the shortcut or the name with a hyphen
+				// added, use that argument
+				if (args[i].equals("-" + pa.name_)
+						|| args[i].equals(pa.shortcut_)) {
+					i++;
+					pa.setValue(args[i]);
+					break;
+				}
+			}
+		}
+		return i;
+	}
+
+	/**
 	 * Loads the program arguments from file.
 	 * 
 	 * @param argFile
@@ -146,7 +200,7 @@ public enum ProgramArgument implements Serializable {
 	public static void loadArgs() {
 		if (!ARG_FILE.exists())
 			return;
-		
+
 		Map<String, String> args = new HashMap<String, String>();
 		try {
 			FileReader fr = new FileReader(ARG_FILE);
@@ -154,8 +208,11 @@ public enum ProgramArgument implements Serializable {
 
 			String input = null;
 			while ((input = br.readLine()) != null) {
-				String[] split = input.split("%")[0].split("=");
-				args.put(split[0], split[1].trim());
+				String[] split = input.split("%");
+				if (split.length > 1) {
+					split = split[0].split("=");
+					args.put(split[0], split[1].trim());
+				}
 			}
 
 			br.close();
@@ -180,12 +237,56 @@ public enum ProgramArgument implements Serializable {
 	}
 
 	/**
+	 * Saves the arguments into a give {@link BufferedWriter}.
+	 * 
+	 * @param bw
+	 *            The buffered writer.
+	 */
+	public static void saveArgs(BufferedWriter bw) throws IOException {
+		SortedSet<ProgramArgument> sortedArgs = new TreeSet<ProgramArgument>(
+				new Comparator<ProgramArgument>() {
+					@Override
+					public int compare(ProgramArgument o1, ProgramArgument o2) {
+						int result = o1.parameterType_
+								.compareTo(o2.parameterType_);
+						if (result != 0)
+							return result;
+						return o1.name_.compareTo(o2.name_);
+					}
+				});
+		for (ProgramArgument pa : ProgramArgument.values()) {
+			sortedArgs.add(pa);
+		}
+
+		ParameterType pt = null;
+		for (ProgramArgument pa : sortedArgs) {
+			// Outline the parameter type
+			if (!pa.parameterType_.equals(pt)) {
+				if (pt != null)
+					bw.write("\n");
+				pt = pa.parameterType_;
+				bw.write("\t-----" + pt + "-----\n");
+			}
+			
+			if (!pa.getValue().equals(pa.getDefaultValue()))
+				bw.write(pa.getName() + "=" + pa.getValue()
+						+ "\t\t\t% -----MODIFIED----- % " + pa.getComment()
+						+ "\n");
+			else
+				bw.write(pa.getName() + "=" + pa.getValue() + "\t\t\t% "
+						+ pa.getComment() + "\n");
+		}
+	}
+
+	/**
 	 * Saves the arguments into a file if the file doesn't already exist.
 	 */
 	public static void saveArgsFile() {
 		try {
 			if (!ARG_FILE.exists())
 				ARG_FILE.createNewFile();
+			else
+				return;
 
 			FileWriter fw = new FileWriter(ARG_FILE);
 			BufferedWriter bw = new BufferedWriter(fw);
@@ -200,59 +301,11 @@ public enum ProgramArgument implements Serializable {
 	}
 
 	/**
-	 * Saves the arguments into a give {@link BufferedWriter}.
+	 * Simple class for dividing the program args into categories.
 	 * 
-	 * @param bw
-	 *            The buffered writer.
+	 * @author Sam Sarjant
 	 */
-	public static void saveArgs(BufferedWriter bw) throws IOException {
-		for (ProgramArgument pa : ProgramArgument.values()) {
-			if (!pa.getValue().equals(pa.getDefaultValue()))
-				bw.write(pa.getName() + "=" + pa.getValue()
-						+ "\t\t\t% -----MODIFIED----- % " + pa.getComment() + "\n");
-			else
-				bw.write(pa.getName() + "=" + pa.getValue() + "\t\t\t% "
-						+ pa.getComment() + "\n");
-		}
-	}
-
-	/**
-	 * Handles a program argument not caught by the algorithm earlier.
-	 * 
-	 * @param i
-	 *            The current index.
-	 * @param args
-	 *            The arguments given at command-line.
-	 * @return The index after handling.
-	 */
-	public static int handleArg(int i, String[] args) {
-		if (args[i].equals("-slotProb")) {
-			i++;
-			if (args[i].equals("dynamic"))
-				INITIAL_SLOT_MEAN.setDoubleValue(-1);
-			else
-				INITIAL_SLOT_MEAN.setDoubleValue(Double.parseDouble(args[i]));
-		} else if (args[i].equals("-ensemble")) {
-			ENSEMBLE_EVALUATION.setBooleanValue(true);
-			i++;
-			ENSEMBLE_SIZE.setDoubleValue(Integer.parseInt(args[i]));
-		} else if (args[i].equals("-dynamicSlots")) {
-			i++;
-			DYNAMIC_SLOTS.setValue(args[i]);
-			LOCAL_ALPHA.setBooleanValue(false);
-		} else if (args[i].charAt(0) == '-') {
-			// Check the arg against the rest of the program args
-			for (ProgramArgument pa : ProgramArgument.values()) {
-				// If the arg equals the shortcut or the name with a hyphen
-				// added, use that argument
-				if (args[i].equals("-" + pa.name_)
-						|| args[i].equals(pa.shortcut_)) {
-					i++;
-					pa.setValue(args[i]);
-					break;
-				}
-			}
-		}
-		return i;
+	private enum ParameterType {
+		CONVERGENCE, EVALUATION, SAMPLING, SPECIALISATION, UPDATING;
 	}
 }
